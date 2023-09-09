a7c22caf9530a4dcc733423c80d1d86a717097d6
BaoNguyen <124386348+Wattatrick@users.noreply.github.com>
2023-09-09 00:53:56 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+let scoreBox = document.getElementById("score-box")
+let scoreBoxguest = document.getElementById("score-box-guest")
+let announCement = document.getElementById("announcement")
+let point = 0
+let pointg = 0
+let announ = "ㅤ"
 
-function increment() {
-    count += 1
-    countEl.textContent = count
+function add(){
+    point += 1
+    scoreBox.textContent = point
 }
-function decrement() {
-    count -= 1
-    countEl.textContent = count
+function add2(){
+    point += 2
+    scoreBox.textContent = point
 }
-function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
+function add3(){
+    point += 3
+    scoreBox.textContent = point
+}
+function addg(){
+    pointg += 1
+    scoreBoxguest.textContent = pointg
+}
+function add2g(){
+    pointg += 2
+    scoreBoxguest.textContent = pointg
+}
+function add3g(){
+    pointg += 3
+    scoreBoxguest.textContent = pointg
 }
 function reset(){
-    countEl.textContent = 0
-    count = 0
-    
+    scoreBox.textContent = 0 
+    scoreBoxguest.textContent = 0
+    point = 0
+    pointg = 0
+}
+function endgame(){
+    if (point > pointg){
+        announ = "The Home Team Has Won The Match 🏆"
+    }else if (point < pointg){
+        announ = "The Guest Team Has Won The Match 🏆"
+    }else {
+        announ = "It's A Draw 🤝"
+    }
+    announCement.textContent = announ
 }
